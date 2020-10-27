1af13070164e1c3167572d53a90b2833e9a6fcc9
Michael Ryan <ryanabu@bc.edu>
2020-10-27 11:26:21 -0400

    Ch. 8.3

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Michael Ryan on 10/27/20.
+//  Copyright © 2020 Michael Ryan. All rights reserved.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
