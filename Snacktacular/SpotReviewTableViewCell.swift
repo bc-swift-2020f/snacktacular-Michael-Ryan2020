ed6fde682ab693ff4052e62753bfcb9bd83dc244
Michael Ryan <ryanabu@bc.edu>
2020-11-08 14:02:39 -0500

    Ch. 8.16

--- Snacktacular/SpotReviewTableViewCell.swift
+++ Snacktacular/SpotReviewTableViewCell.swift
@@ -14,10 +14,19 @@ class SpotReviewTableViewCell: UITableViewCell {
     
     @IBOutlet weak var reviewTextLabel: UILabel!
     
+    @IBOutlet var starImageCollection: [UIImageView]!
+    
     var review: Review! {
         didSet {
             reviewTitleLabel.text = review.title
             reviewTextLabel.text = review.text
+            for starImage in starImageCollection {
+                let imageName = (starImage.tag < review.rating ? "star.fill" : "star")
+                starImage.image = UIImage(systemName: imageName)
+
+                starImage.tintColor = (starImage.tag < review.rating ? .systemRed : .darkText)
+                }
+            }
         }
     }
-}
+
